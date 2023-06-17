a0a515ea96b1e5a29a4dd833fd18c7b2c5dc3620
Yan0606 <yan060604@gmail.com>
2023-06-17 10:38:36 -0300

    modifications about js

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -1,13 +1,10 @@
-/* Template: Blink SaaS App Website Bootstrap HTML Template
-   Description: Custom JS file
-*/
+
 
 
 (function($) {
     "use strict"; 
 	
-    /* Navbar Scripts */
-    // jQuery to collapse the navbar on scroll
+    
     $(window).on('scroll load', function() {
 		if ($(".navbar").offset().top > 60) {
 			$(".fixed-top").addClass("top-nav-collapse");
@@ -16,7 +13,7 @@
 		}
     });
     
-	// jQuery for page scrolling feature - requires jQuery Easing plugin
+	
 	$(function() {
 		$(document).on('click', 'a.page-scroll', function(event) {
 			var $anchor = $(this);
@@ -27,12 +24,12 @@
 		});
     });
 
-    // offcanvas script from Bootstrap + added element to close menu on click in small viewport
+   
     $('[data-toggle="offcanvas"], .navbar-nav li a:not(.dropdown-toggle').on('click', function () {
         $('.offcanvas-collapse').toggleClass('open')
     })
 
-    // hover in desktop mode
+    
     function toggleDropdown (e) {
         const _d = $(e.target).closest('.dropdown'),
             _m = $('.dropdown-menu', _d);
@@ -48,7 +45,7 @@
     .on('click', '.dropdown-menu a', toggleDropdown);
 
 
-    /* Details Lightbox - Magnific Popup */
+ 
     $('.popup-with-move-anim').magnificPopup({
 		type: 'inline',
 		fixedContentPos: true,
@@ -62,7 +59,7 @@
 	});
 	
 
-	/* Image Slider - Swiper */
+	
     var imageSlider = new Swiper('.image-slider', {
         autoplay: {
             delay: 3000,
@@ -72,22 +69,22 @@
         spaceBetween: 50,
         slidesPerView: 5,
 		breakpoints: {
-            // when window is <= 575px
+           
             575: {
                 slidesPerView: 1,
                 spaceBetween: 10
             },
-            // when window is <= 767px
+            
             767: {
                 slidesPerView: 2,
                 spaceBetween: 20
             },
-            // when window is <= 991px
+          
             991: {
                 slidesPerView: 3,
                 spaceBetween: 20
             },
-            // when window is <= 1199px
+           
             1199: {
                 slidesPerView: 4,
                 spaceBetween: 20
@@ -97,7 +94,7 @@
     });
     
 
-    /* Card Slider - Swiper */
+   
 	var cardSlider = new Swiper('.card-slider', {
 		autoplay: {
             delay: 5000,
@@ -111,7 +108,7 @@
 		slidesPerView: 2,
 		spaceBetween: 40,
         breakpoints: {
-            // when window is <= 991px
+           
             991: {
                 slidesPerView: 1
             }
@@ -119,10 +116,10 @@
     });
 
 
-    /* Counter - CountTo */
+   
 	var a = 0;
 	$(window).scroll(function() {
-		if ($('#counter').length) { // checking if CountTo section exists in the page, if not it will not run the script and avoid errors	
+		if ($('#counter').length) { 	
 			var oTop = $('#counter').offset().top - window.innerHeight;
 			if (a == 0 && $(window).scrollTop() > oTop) {
 			$('.counter-value').each(function() {
@@ -141,7 +138,6 @@
 					},
 					complete: function() {
 					$this.text(this.countNum);
-					//alert('finished');
 					}
 				});
 			});
@@ -151,8 +147,7 @@
     });
 
 
-    /* Move Form Fields Label When User Types */
-    // for input and textarea fields
+  
     $("input, textarea").keyup(function(){
 		if ($(this).val() != '') {
 			$(this).addClass('notEmpty');
@@ -162,8 +157,7 @@
 	});
 	
 
-    /* Back To Top Button */
-    // create the back to top button
+   
     $('body').prepend('<a href="body" class="back-to-top page-scroll">Back to Top</a>');
     var amountScrolled = 700;
     $(window).scroll(function() {
@@ -175,7 +169,7 @@
     });
 
 
-	/* Removes Long Focus On Buttons */
+	
 	$(".button, a, button").mouseup(function() {
 		$(this).blur();
 	});
