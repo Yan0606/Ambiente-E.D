68006e88fcf125eb61b9db3d78a689721f9f32d4
adryanzx <adryan_freire@hotmail.com>
2023-09-05 16:09:25 -0300

    QUIZ

--- web/js/questions.js
+++ web/js/questions.js
@@ -1,12 +1,12 @@
 let questions = [
     {
-    numb: 1,
-    question: "pergunta",
-    answer: "resposta",
+    numb: 1,  //NUMERO DA QUESTÃO
+    question: "1+1", //COLOCAR PERGUNTA
+    answer: "2", //COLOCAR RESPOSTA DA PERGUNTA
     options: [
+      "1",                   //COLOCAR OPÇÕES DE RESPOSTA E ENTRE ELAS A RESPOSTA CORRETA DA PERGUNTA QUE ESTÁ LOGO ACIMA!
       "1",
-      "1",
-      "1",
+      "2",
       "1"
     ]
   },
--- web/js/script.js
+++ web/js/script.js
@@ -1,4 +1,4 @@
-//selecting all required elements
+// ELEMENTOS
 const start_btn = document.querySelector(".start_btn button");
 const info_box = document.querySelector(".info_box");
 const exit_btn = info_box.querySelector(".buttons .quit");
